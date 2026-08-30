7c6bd96e5838808d2181ef0a8cad65a28f4c50df
Md Mosabbeer Hossain <139885579+mosabbeer@users.noreply.github.com>
2026-08-31 01:12:17 +0600

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string,vector<string>>temp;
+        for(int i=0;i<strs.size();i++)
+        {
+            
+            string str=strs[i];
+            sort(str.begin(),str.end());
+            temp[str].push_back(strs[i]);
+        }
+        vector<vector<string>>ans;
+        for(auto it:temp)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
